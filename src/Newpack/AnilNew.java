0570326ff05c22204ef205d516744a65bc20de3b
opentrends <opentrends@DESKTOP-VMNP2HD>
2021-09-13 17:20:35 +0530

    Second comm

--- src/Newpack/AnilNew.java
+++ src/Newpack/AnilNew.java
@@ -5,7 +5,7 @@ public class AnilNew {
 	public static void main(String[] args) {
 		
 		
-		System.out.println("Abcd");
+		System.out.println("Abcd"); //weqweqweqwee
 		
 		// TODO Auto-generated method stub
 
